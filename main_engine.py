dc7d1af45c16a1da1bf9cb115823d4987fc8b145
Dogmeal30 <gig.giorgadze@gmail.com>
2026-05-01 02:35:43 +0400

    Update main_engine.py

--- main_engine.py
+++ main_engine.py
@@ -1,12 +1,12 @@
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-return {"status": "DOGMEAL Online", "message": "სისტემა მზად არის!"}
+        return {"status": "Online", "message": "Jemal is here!"}
 
-@app.get("/test")
-def test():
-return {"health": "ok"}
+
+
 
